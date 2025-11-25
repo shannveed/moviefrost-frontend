90eb21d85445c839a6eb6c1f6fd8f4f19be2d92f
shannaveed <shanaveed10@gmail.com>
2025-11-26 02:23:08 +0500

    Update backend for Vercel deployment

--- src/Components/Home/KoreanDramaSection.js
+++ src/Components/Home/KoreanDramaSection.js
@@ -114,50 +114,50 @@ function KoreanDramaSection() {
 
           <div className="hidden sm:block relative">
             <Swiper
-              modules={[Navigation, Autoplay]}
-              navigation={{
-                prevEl: prevEl.current,
-                nextEl: nextEl.current,
-              }}
-              onBeforeInit={(swiper) => {
-                swiper.params.navigation.prevEl = prevEl.current;
-                swiper.params.navigation.nextEl = nextEl.current;
-              }}
-              autoplay={{ delay: 3000, disableOnInteraction: false }}
-              loop
-              speed={200}
-              spaceBetween={15}
-              slidesPerView={2}
-              breakpoints={{
-                640: { slidesPerView: 2, spaceBetween: 10 },
-                768: { slidesPerView: 3, spaceBetween: 15 },
-                1024: { slidesPerView: 4, spaceBetween: 20 },
-                1280: { slidesPerView: 5, spaceBetween: 20 },
-              }}
-            >
-              {movies.slice(0, 20).map((movie) => (
-                <SwiperSlide key={movie._id}>
-                  <Movie movie={movie} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-
-            <button
-              ref={prevEl}
-              aria-label="Previous"
-              className="absolute left-0 top-1/2 -translate-y-1/2 z-20 w-8 h-8 mobile:w-mobile-arrow mobile:h-mobile-arrow flex-colo
-                        bg-customPurple/70 hover:bg-customPurple text-white rounded-full mobile:rounded-md"
-            >
-              <BsCaretLeftFill />
-            </button>
-            <button
-              ref={nextEl}
-              aria-label="Next"
-              className="absolute right-0 top-1/2 -translate-y-1/2 z-20 w-8 h-8 mobile:w-mobile-arrow mobile:h-mobile-arrow flex-colo
-                        bg-customPurple/70 hover:bg-customPurple text-white rounded-full mobile:rounded-md"
-            >
-              <BsCaretRightFill />
-            </button>
+                          modules={[Navigation, Autoplay]}
+                          navigation={{
+                            prevEl: prevEl.current,
+                            nextEl: nextEl.current,
+                          }}
+                          onBeforeInit={(swiper) => {
+                            swiper.params.navigation.prevEl = prevEl.current;
+                            swiper.params.navigation.nextEl = nextEl.current;
+                          }}
+                          autoplay={{ delay: 3000, disableOnInteraction: false }}
+                          loop
+                          speed={200}
+                          spaceBetween={15}
+                          slidesPerView={2}
+                          breakpoints={{
+                            640: { slidesPerView: 2, spaceBetween: 10 },
+                            768: { slidesPerView: 3, spaceBetween: 15 },
+                            1024: { slidesPerView: 4, spaceBetween: 20 },
+                            1280: { slidesPerView: 5, spaceBetween: 20 },
+                          }}
+                        >
+                          {movies.slice(0, 20).map((movie) => (
+                            <SwiperSlide key={movie._id}>
+                              <Movie movie={movie} />
+                            </SwiperSlide>
+                          ))}
+                        </Swiper>
+            
+                        <button
+                          ref={prevEl}
+                          aria-label="Previous"
+                          className="absolute left-0 top-1/2 -translate-y-1/2 z-20 w-8 h-8 mobile:w-mobile-arrow mobile:h-mobile-arrow flex-colo
+                                    bg-customPurple/70 hover:bg-customPurple text-white rounded-full mobile:rounded-md"
+                        >
+                          <BsCaretLeftFill />
+                        </button>
+                        <button
+                          ref={nextEl}
+                          aria-label="Next"
+                          className="absolute right-0 top-1/2 -translate-y-1/2 z-20 w-8 h-8 mobile:w-mobile-arrow mobile:h-mobile-arrow flex-colo
+                                    bg-customPurple/70 hover:bg-customPurple text-white rounded-full mobile:rounded-md"
+                        >
+                          <BsCaretRightFill />
+                        </button>
           </div>
         </>
       ) : (
